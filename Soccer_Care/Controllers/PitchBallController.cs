7caca7396d1739b16cbdb6c511bf7094f8160762
NguyenMinhChien <minhchien03112003@gmail.com>
2024-03-24 22:01:01 +0700

    form

--- Soccer_Care/Controllers/PitchBallController.cs
+++ Soccer_Care/Controllers/PitchBallController.cs
@@ -1,16 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Soccer_Care.Models;
 
 namespace Soccer_Care.Controllers
 {
     public class PitchBallController : Controller
     {
         IHttpContextAccessor contextAccessor;
-        public PitchBallController(IHttpContextAccessor contextAccessor) {
+        private readonly SoccerCareDbContext _context;
+        public PitchBallController(IHttpContextAccessor contextAccessor, SoccerCareDbContext context) {
             this.contextAccessor = contextAccessor;
+            this._context = context;
         }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult DatSan(string id)
+        {
+            var pitch = _context.FootBallFields.FirstOrDefault(f => f.IDFootBallField == id);
+            return View(pitch);
+        }
+        
+
+        public IActionResult DatSanConfirm()
+        {
+           
+            return View("Thanks");
         }
     }
 }
